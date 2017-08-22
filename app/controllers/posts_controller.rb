e60e23085b18e354c5d5c2ae3a06790fd38ec3e1
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-08-22 11:25:53 -0400

    added crud methods to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,31 @@ def index
   render json: @posts
 end
 
+def create
+  @post = Post.create!(entry_params)
+  render json: @post
+end
+
 def show
   @post = Post.find(params[:id])
   render json: @post.to_json, status: :ok
 end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update!(entry_params)
+  render json: @post
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  render nothing:true
+end
+
+private
+  def entry_params
+    params.require(:post).permit(:content)
+  end
 
 end
